0dc82f34abf71a3024728cc5f21cac90d5b494cd
Matthew LeLiever <matthew121250@hotmail.com>
2019-05-03 14:53:38 -0400

    Refactors

--- SecretEncryptor/Program.cs
+++ SecretEncryptor/Program.cs
@@ -14,13 +14,13 @@ namespace SecretEncryptor
         {
             //ReadDrives();
 
-            EncryptData();
-            DecryptData();
+            DoEncryption();
+            DoDecryption();
 
             Console.ReadKey();
         }
 
-        private static void DecryptData()
+        private static void DoDecryption()
         {
             Console.WriteLine("Enter secret name/type:");
 
@@ -30,22 +30,19 @@ namespace SecretEncryptor
 
             string key = Console.ReadLine();
 
-            using (SecureDataEncryptor dataEncryptor = new SecureDataEncryptor(new AdvancedSecureRandom(new Blake2bDigest(512), RandomString.Secure.Blake2.GetString(key, 64))))
+            for (int i = 0; i < 4; i++)
             {
-                for (int i = 0; i < 4; i++)
+                using (StreamReader readtext = new StreamReader(name + "_secret_" + (i + 1) + ".txt"))
                 {
-                    using (StreamReader readtext = new StreamReader(name + "_secret_" + (i + 1) + ".txt"))
-                    {
-                        string ciphertext = readtext.ReadLine();
-                        string decryptedSecret = dataEncryptor.Decrypt(ciphertext, RandomString.Secure.SHA3.GetString(key, 64));
+                    string ciphertext = readtext.ReadLine();
+                    string decryptedSecret = Decrypt(ciphertext, key);
 
-                        Console.WriteLine("\nSecret #" + (i + 1) + ": " + decryptedSecret + "\n");
-                    }
+                    Console.WriteLine("\nSecret #" + (i + 1) + ": " + decryptedSecret + "\n");
                 }
             }
         }
 
-        private static void EncryptData()
+        private static void DoEncryption()
         {
             Console.WriteLine("Enter secret name/type:");
 
@@ -55,22 +52,35 @@ namespace SecretEncryptor
 
             string key = Console.ReadLine();
 
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("\nEnter secret #" + (i + 1) + ":");
+
+                string secret = Console.ReadLine();
+                string encryptedSecret = Encrypt(secret, key);
+
+                Console.WriteLine("\nSecret #" + (i + 1) + " encrypted: " + encryptedSecret + "\n");
+
+                using (StreamWriter writer = new StreamWriter(name + "_secret_" + (i + 1) + ".txt"))
+                {
+                    writer.Write(encryptedSecret);
+                }
+            }
+        }
+
+        private static string Encrypt(string secret, string key)
+        {
             using (SecureDataEncryptor dataEncryptor = new SecureDataEncryptor(new AdvancedSecureRandom(new Blake2bDigest(512), RandomString.Secure.Blake2.GetString(key, 64))))
             {
-                for (int i = 0; i < 4; i++)
-                {
-                    Console.WriteLine("\nEnter secret #" + (i + 1) + ":");
+                return dataEncryptor.Encrypt(secret, RandomString.Secure.SHA3.GetString(key, 64));
+            }
+        }
 
-                    string secret = Console.ReadLine();
-                    string encryptedSecret = dataEncryptor.Encrypt(secret, RandomString.Secure.SHA3.GetString(key, 64));
-
-                    Console.WriteLine("\nSecret #" + (i + 1) + " encrypted: " + encryptedSecret + "\n");
-
-                    using (StreamWriter writer = new StreamWriter(name + "_secret_" + (i + 1) + ".txt"))
-                    {
-                        writer.Write(encryptedSecret);
-                    }
-                }
+        private static string Decrypt(string ciphertext, string key)
+        {
+            using (SecureDataEncryptor dataEncryptor = new SecureDataEncryptor(new AdvancedSecureRandom(new Blake2bDigest(512), RandomString.Secure.Blake2.GetString(key, 64))))
+            {
+                return dataEncryptor.Decrypt(ciphertext, RandomString.Secure.SHA3.GetString(key, 64));
             }
         }
 
